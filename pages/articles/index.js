fcb5089b27ac3cc923f3e9a67c504ce680a0931c
Thomas Misikoff <tmisikoff@gmail.com>
2021-11-06 14:39:05 -0600

    feat: include tailwind and a few routes

---
+++ pages/articles/index.js
@@ -0,0 +1,20 @@
+import Head from 'next/head'
+import Layout from '../../components/layout'
+
+export default function Home() {
+  return ( 
+    <Layout>
+      <div className="flex flex-col items-center justify-center min-h-screen py-2">
+        <Head>
+          <title>Articles</title>
+        </Head>
+
+        <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
+          
+        </main>
+
+        
+      </div>
+    </Layout>
+  )
+}
