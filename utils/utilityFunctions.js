9dc5b6458a3cb580ed4a699a51fae75691ed2356
Thomas Misikoff <tmisikoff@gmail.com>
2021-11-08 16:50:52 -0700

    feat: added globe to home page

---
+++ utils/utilityFunctions.js
@@ -0,0 +1,14 @@
+const utilityFunctions = {
+  shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1))
+      ;[array[i], array[j]] = [array[j], array[i]]
+    }
+    return array
+  },
+  sleep(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms))
+  },
+}
+
+export default utilityFunctions
