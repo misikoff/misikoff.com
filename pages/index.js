82244d680a94d7f40d77f180efb105c4ee2611e5
Thomas Misikoff <tmisikoff@gmail.com>
2021-11-07 17:57:54 -0700

    fix: style improvements

--- pages/index.js
+++ pages/index.js
@@ -1,284 +1,130 @@
+import Link from 'next/link'
 import Layout from 'components/layout'
-
-/* This example requires Tailwind CSS v2.0+ */
-import {
-  AnnotationIcon,
-  GlobeAltIcon,
-  LightningBoltIcon,
-  MailIcon,
-  ScaleIcon,
-} from '@heroicons/react/outline'
-
-const transferFeatures = [
-  {
-    id: 1,
-    name: 'Competitive exchange rates',
-    description:
-      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.',
-    icon: GlobeAltIcon,
-  },
-  {
-    id: 2,
-    name: 'No hidden fees',
-    description:
-      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.',
-    icon: ScaleIcon,
-  },
-  {
-    id: 3,
-    name: 'Transfers are instant',
-    description:
-      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.',
-    icon: LightningBoltIcon,
-  },
-]
-const communicationFeatures = [
-  {
-    id: 1,
-    name: 'Mobile notifications',
-    description:
-      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.',
-    icon: AnnotationIcon,
-  },
-  {
-    id: 2,
-    name: 'Reminder emails',
-    description:
-      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.',
-    icon: MailIcon,
-  },
-]
 
 export default function Example() {
   return (
     <Layout>
-      <div className="py-16 bg-gray-50 overflow-hidden lg:py-24">
-        <div className="relative mx-auto px-4 max-w-xl sm:px-6 lg:px-8 lg:max-w-7xl">
-          <svg
-            className="absolute left-full hidden transform -translate-x-1/2 -translate-y-1/4 lg:block"
-            width={404}
-            height={784}
-            fill="none"
-            viewBox="0 0 404 784"
-            aria-hidden="true"
-          >
-            <defs>
-              <pattern
-                id="b1e6e422-73f8-40a6-b5d9-c8586e37e0e7"
-                x={0}
-                y={0}
-                width={20}
-                height={20}
-                patternUnits="userSpaceOnUse"
-              >
-                <rect
-                  x={0}
-                  y={0}
-                  width={4}
-                  height={4}
-                  className="text-gray-200"
-                  fill="currentColor"
-                />
-              </pattern>
-            </defs>
-            <rect
-              width={404}
-              height={784}
-              fill="url(#b1e6e422-73f8-40a6-b5d9-c8586e37e0e7)"
-            />
-          </svg>
-
-          <div className="relative">
-            <h2 className="text-center text-gray-900 text-3xl font-extrabold tracking-tight leading-8 sm:text-4xl">
-              A better way to send money
-            </h2>
-            <p className="mt-4 mx-auto max-w-3xl text-center text-gray-500 text-xl">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus
-              magnam voluptatum cupiditate veritatis in, accusamus quisquam.
-            </p>
-          </div>
-
-          <div className="relative mt-12 lg:grid lg:gap-8 lg:grid-cols-2 lg:items-center lg:mt-24">
-            <div className="relative">
-              <h3 className="text-gray-900 text-2xl font-extrabold tracking-tight sm:text-3xl">
-                Transfer funds world-wide
-              </h3>
-              <p className="mt-3 text-gray-500 text-lg">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Pariatur minima sequi recusandae, porro maiores officia
-                assumenda aliquam laborum ab aliquid veritatis impedit odit
-                adipisci optio iste blanditiis facere. Totam, velit.
-              </p>
-
-              <dl className="mt-10 space-y-10">
-                {transferFeatures.map((item) => (
-                  <div key={item.id} className="relative">
-                    <dt>
-                      <div className="absolute flex items-center justify-center w-12 h-12 text-white bg-indigo-500 rounded-md">
-                        <item.icon className="w-6 h-6" aria-hidden="true" />
-                      </div>
-                      <p className="ml-16 text-gray-900 text-lg font-medium leading-6">
-                        {item.name}
-                      </p>
-                    </dt>
-                    <dd className="ml-16 mt-2 text-gray-500 text-base">
-                      {item.description}
-                    </dd>
-                  </div>
-                ))}
-              </dl>
-            </div>
-
-            <div className="relative -mx-4 mt-10 lg:mt-0" aria-hidden="true">
-              <svg
-                className="absolute left-1/2 transform -translate-x-1/2 translate-y-16 lg:hidden"
-                width={784}
-                height={404}
-                fill="none"
-                viewBox="0 0 784 404"
-              >
-                <defs>
-                  <pattern
-                    id="ca9667ae-9f92-4be7-abcb-9e3d727f2941"
-                    x={0}
-                    y={0}
-                    width={20}
-                    height={20}
-                    patternUnits="userSpaceOnUse"
-                  >
-                    <rect
-                      x={0}
-                      y={0}
-                      width={4}
-                      height={4}
-                      className="text-gray-200"
-                      fill="currentColor"
-                    />
-                  </pattern>
-                </defs>
-                <rect
-                  width={784}
-                  height={404}
-                  fill="url(#ca9667ae-9f92-4be7-abcb-9e3d727f2941)"
-                />
-              </svg>
-              <img
-                className="relative mx-auto"
-                width={490}
-                src="https://tailwindui.com/img/features/feature-example-1.png"
-                alt=""
-              />
-            </div>
-          </div>
-
-          <svg
-            className="absolute right-full hidden transform translate-x-1/2 translate-y-12 lg:block"
-            width={404}
-            height={784}
-            fill="none"
-            viewBox="0 0 404 784"
-            aria-hidden="true"
-          >
-            <defs>
-              <pattern
-                id="64e643ad-2176-4f86-b3d7-f2c5da3b6a6d"
-                x={0}
-                y={0}
-                width={20}
-                height={20}
-                patternUnits="userSpaceOnUse"
-              >
-                <rect
-                  x={0}
-                  y={0}
-                  width={4}
-                  height={4}
-                  className="text-gray-200"
-                  fill="currentColor"
-                />
-              </pattern>
-            </defs>
-            <rect
-              width={404}
-              height={784}
-              fill="url(#64e643ad-2176-4f86-b3d7-f2c5da3b6a6d)"
-            />
-          </svg>
-
-          <div className="relative mt-12 sm:mt-16 lg:mt-24">
-            <div className="lg:grid lg:gap-8 lg:grid-flow-row-dense lg:grid-cols-2 lg:items-center">
-              <div className="lg:col-start-2">
-                <h3 className="text-gray-900 text-2xl font-extrabold tracking-tight sm:text-3xl">
-                  Always in the loop
-                </h3>
-                <p className="mt-3 text-gray-500 text-lg">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Impedit ex obcaecati natus eligendi delectus, cum deleniti
-                  sunt in labore nihil quod quibusdam expedita nemo.
-                </p>
-
-                <dl className="mt-10 space-y-10">
-                  {communicationFeatures.map((item) => (
-                    <div key={item.id} className="relative">
-                      <dt>
-                        <div className="absolute flex items-center justify-center w-12 h-12 text-white bg-indigo-500 rounded-md">
-                          <item.icon className="w-6 h-6" aria-hidden="true" />
-                        </div>
-                        <p className="ml-16 text-gray-900 text-lg font-medium leading-6">
-                          {item.name}
-                        </p>
-                      </dt>
-                      <dd className="ml-16 mt-2 text-gray-500 text-base">
-                        {item.description}
-                      </dd>
-                    </div>
-                  ))}
-                </dl>
-              </div>
-
-              <div className="relative -mx-4 mt-10 lg:col-start-1 lg:mt-0">
-                <svg
-                  className="absolute left-1/2 transform -translate-x-1/2 translate-y-16 lg:hidden"
-                  width={784}
-                  height={404}
-                  fill="none"
-                  viewBox="0 0 784 404"
-                  aria-hidden="true"
+      <div className="relative overflow-hidden">
+        <div
+          className="hidden sm:absolute sm:inset-y-0 sm:block sm:w-full sm:h-full"
+          aria-hidden="true"
+        >
+          <div className="relative mx-auto max-w-7xl h-full">
+            <svg
+              className="absolute right-full transform translate-x-1/4 translate-y-1/4 lg:translate-x-1/2"
+              width="404"
+              height="784"
+              fill="none"
+              viewBox="0 0 404 784"
+            >
+              <defs>
+                <pattern
+                  id="f210dbf6-a58d-4871-961e-36d5016a0f49"
+                  x="0"
+                  y="0"
+                  width="20"
+                  height="20"
+                  patternUnits="userSpaceOnUse"
                 >
-                  <defs>
-                    <pattern
-                      id="e80155a9-dfde-425a-b5ea-1f6fadd20131"
-                      x={0}
-                      y={0}
-                      width={20}
-                      height={20}
-                      patternUnits="userSpaceOnUse"
-                    >
-                      <rect
-                        x={0}
-                        y={0}
-                        width={4}
-                        height={4}
-                        className="text-gray-200"
-                        fill="currentColor"
-                      />
-                    </pattern>
-                  </defs>
                   <rect
-                    width={784}
-                    height={404}
-                    fill="url(#e80155a9-dfde-425a-b5ea-1f6fadd20131)"
+                    x="0"
+                    y="0"
+                    width="4"
+                    height="4"
+                    className="text-gray-200"
+                    fill="currentColor"
                   />
-                </svg>
-                <img
-                  className="relative mx-auto"
-                  width={490}
-                  src="https://tailwindui.com/img/features/feature-example-2.png"
-                  alt=""
-                />
-              </div>
-            </div>
+                </pattern>
+              </defs>
+              <rect
+                width="404"
+                height="784"
+                fill="url(#f210dbf6-a58d-4871-961e-36d5016a0f49)"
+              />
+            </svg>
+            <svg
+              className="absolute left-full transform -translate-x-1/4 -translate-y-3/4 md:-translate-y-1/2 lg:-translate-x-1/2"
+              width="404"
+              height="784"
+              fill="none"
+              viewBox="0 0 404 784"
+            >
+              <defs>
+                <pattern
+                  id="5d0dd344-b041-4d26-bec4-8d33ea57ec9b"
+                  x="0"
+                  y="0"
+                  width="20"
+                  height="20"
+                  patternUnits="userSpaceOnUse"
+                >
+                  <rect
+                    x="0"
+                    y="0"
+                    width="4"
+                    height="4"
+                    className="text-gray-200"
+                    fill="currentColor"
+                  />
+                </pattern>
+              </defs>
+              <rect
+                width="404"
+                height="784"
+                fill="url(#5d0dd344-b041-4d26-bec4-8d33ea57ec9b)"
+              />
+            </svg>
           </div>
+        </div>
+
+        <div className="relative pb-16 pt-6 sm:pb-24">
+          <div className="mx-auto px-4 max-w-7xl sm:px-6" />
+
+          <main className="mx-auto px-4 max-w-7xl sm:mt-0">
+            <div className="text-center">
+              <h1 className="text-gray-900 text-4xl font-extrabold tracking-tight sm:text-5xl md:text-6xl">
+                <span className="">Thomas Misikoff</span>
+                <div className="flex flex-row items-center justify-center text-blue-600 text-2xl sm:text-3xl md:text-4xl">
+                  <span> software engineer </span>
+
+                  <span className="px-2 text-black"> • </span>
+
+                  <span> statistical analyst </span>
+                </div>
+              </h1>
+              <p className="mt-3 mx-auto max-w-md text-left text-gray-500 text-base sm:text-lg md:mt-8 md:max-w-3xl md:text-xl">
+                This is a growing hub where I discuss novel concepts and their
+                applications.
+                <br />
+                <br />
+                Content is broken up into{' '}
+                <span className="px-1 text-blue-600 font-mono">
+                  articles
+                </span>{' '}
+                and{' '}
+                <span className="pl-1 text-blue-600 font-mono">sandboxes</span>.
+                Articles have a narrative focus, often introducing concepts to
+                be explored experimentally through sandboxes.
+              </p>
+              <div className="mt-8 mx-auto max-w-md sm:flex sm:justify-center md:mt-8">
+                <div className="rounded-md shadow">
+                  <Link href="/articles">
+                    <a className="flex items-center justify-center px-8 py-3 w-full text-white text-base font-medium bg-blue-600 hover:bg-blue-700 border border-transparent rounded-md md:px-10 md:py-4 md:text-lg">
+                      Read Articles
+                    </a>
+                  </Link>
+                </div>
+                <div className="mt-3 rounded-md shadow sm:ml-3 sm:mt-0">
+                  <Link href="/sandboxes">
+                    <a className="flex items-center justify-center px-8 py-3 w-full text-blue-600 text-base font-medium hover:bg-gray-50 bg-white border border-transparent rounded-md md:px-10 md:py-4 md:text-lg">
+                      Explore Sandboxes
+                    </a>
+                  </Link>
+                </div>
+              </div>
+              {/* <globe /> */}
+            </div>
+          </main>
         </div>
       </div>
     </Layout>
