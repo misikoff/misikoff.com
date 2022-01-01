8a7b017f762100f9ca1397bcdf8523ef7f7d6b36
Thomas Misikoff <tmisikoff@gmail.com>
2022-01-01 17:54:00 -0600

    fix: icons

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,21 @@
 import { AppProps } from 'next/dist/shared/lib/router/router'
+import Head from 'next/head'
 import 'tailwindcss/tailwind.css'
 import Layout from 'components/layout'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Head>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
+        />
+      </Head>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   )
 }
 
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -15,54 +15,50 @@ class MyDocument extends Document {
           <meta name="description" content="Best PWA App in the world" />
           <meta name="format-detection" content="telephone=no" />
           <meta name="mobile-web-app-capable" content="yes" />
-          <meta
+          {/* <meta
             name="msapplication-config"
             content="/icons/browserconfig.xml"
           />
           <meta name="msapplication-TileColor" content="#3a7ff3" />
-          <meta name="msapplication-tap-highlight" content="no" />
+          <meta name="msapplication-tap-highlight" content="no" /> */}
           <meta name="theme-color" content="#ffffff" />
 
           <link rel="apple-touch-icon" href="/icons-auto/apple-icon-180.png" />
-          {/* <link
+          <link
             rel="apple-touch-icon"
             sizes="152x152"
             href="/icons/touch-icon-ipad.png"
-          /> */}
-          {/* <link
+          />
+          <link
             rel="apple-touch-icon"
             sizes="180x180"
             href="/icons/touch-icon-iphone-retina.png"
-          /> */}
-          {/* <link
+          />
+          <link
             rel="apple-touch-icon"
             sizes="167x167"
             href="/icons/touch-icon-ipad-retina.png"
-          /> */}
+          />
 
-          {/* <link
+          <link
             rel="icon"
             type="image/png"
             sizes="32x32"
             href="/icons/favicon-32x32.png"
-          /> */}
-          {/* <link
+          />
+          <link
             rel="icon"
             type="image/png"
             sizes="16x16"
             href="/icons/favicon-16x16.png"
-          /> */}
+          />
           <link rel="manifest" href="/manifest.json" />
           <link
             rel="mask-icon"
             href="/icons-auto/manifest-icon-192.maskable.png"
-            color="#3a7ff3"
+            // color="#3a7ff3"
           />
-          <link rel="shortcut icon" href="/favicon.ico" />
-          {/* <link
-            rel="stylesheet"
-            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500"
-          /> */}
+          <link rel="icon" href="/favicon.ico" />
 
           <meta name="twitter:card" content="summary" />
           <meta name="twitter:url" content="https://nextjs.misikoff.com" />
@@ -87,7 +83,7 @@ class MyDocument extends Document {
           />
 
           {/* apple splash screen images */}
-          <link
+          {/* <link
             rel="apple-touch-startup-image"
             href="/icons-auto/apple-splash-2048-2732.jpg"
             sizes="2048x2732"
@@ -121,7 +117,7 @@ class MyDocument extends Document {
             rel="apple-touch-startup-image"
             href="/icons-auto/apple-splash-640-1136.jpg"
             sizes="640x1136"
-          />
+          /> */}
         </Head>
         <body className="h-full">
           <Main />
