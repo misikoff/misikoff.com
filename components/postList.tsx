6008c7e14c2622f7c6ce7620ec653930ea111f95
Thomas Misikoff <tmisikoff@gmail.com>
2022-11-25 15:31:20 -0500

    chore: finished upgrade to next 13

--- components/postList.tsx
+++ components/postList.tsx
@@ -26,88 +26,86 @@ export default function PostList({
     <ul className='space-y-8'>
       {posts.map((article) => (
         <li key={article.frontMatter.title}>
-          <Link href={pathPrefix + article.slug}>
-            <a className='group my-4 cursor-pointer'>
-              <div className=' flex w-full max-w-lg flex-col overflow-hidden rounded-lg shadow-md transition-shadow group-hover:shadow-lg'>
-                <div className='w-full flex-shrink-0'>
-                  <TailwindImage
-                    className='h-48 w-full'
-                    src={article.frontMatter.thumbnailUrl}
-                    alt='Article Image'
-                    width={400}
-                    height={200}
-                  />
-                </div>
-                <div className='flex flex-1 flex-col justify-between bg-white p-6'>
-                  <div className='flex-1 text-left'>
-                    {article.frontMatter.tags &&
-                      article.frontMatter.tags.length && (
-                        <div className='inline-flex gap-x-2'>
-                          {article.frontMatter.tags.map((tag) => (
-                            <span
-                              key={tag}
-                              className='rounded-full bg-blue-400 px-2 py-1 text-xs font-semibold tracking-widest text-white'
-                            >
-                              {tag}
-                            </span>
-                          ))}
-                        </div>
-                      )}
-
-                    <div className='mt-2 block'>
-                      <p className='text-xl font-semibold text-gray-900'>
-                        {article.frontMatter.title}
-                      </p>
-                      <p className='mt-3 text-base text-gray-500'>
-                        {article.frontMatter.description}
-                      </p>
+          <Link
+            href={pathPrefix + article.slug}
+            className='group my-4 cursor-pointer'
+          >
+            <div className=' flex w-full max-w-lg flex-col overflow-hidden rounded-lg shadow-md transition-shadow group-hover:shadow-lg'>
+              <div className='w-full flex-shrink-0'>
+                <TailwindImage
+                  className='h-48 w-full'
+                  src={article.frontMatter.thumbnailUrl}
+                  alt='Article Image'
+                />
+              </div>
+              <div className='flex flex-1 flex-col justify-between bg-white p-6'>
+                <div className='flex-1 text-left'>
+                  {article.frontMatter.tags && article.frontMatter.tags.length && (
+                    <div className='inline-flex gap-x-2'>
+                      {article.frontMatter.tags.map((tag) => (
+                        <span
+                          key={tag}
+                          className='rounded-full bg-blue-400 px-2 py-1 text-xs font-semibold tracking-widest text-white'
+                        >
+                          {tag}
+                        </span>
+                      ))}
                     </div>
+                  )}
+
+                  <div className='mt-2 block'>
+                    <p className='text-xl font-semibold text-gray-900'>
+                      {article.frontMatter.title}
+                    </p>
+                    <p className='mt-3 text-base text-gray-500'>
+                      {article.frontMatter.description}
+                    </p>
                   </div>
-                  <div className='mt-6 flex items-center'>
-                    <div className='flex w-full flex-col justify-between space-y-2 text-sm text-gray-500 sm:flex-row sm:space-x-1 sm:space-y-0'>
-                      <div className='space-x-1'>
-                        <time dateTime={article.frontMatter.date}>
-                          {prettyDate(article.frontMatter.date)}
-                        </time>
-                        {article.frontMatter.readingTime && !hideReadTime && (
-                          <>
-                            <span aria-hidden='true'>&middot;</span>
-                            <span>
-                              {Math.ceil(
-                                article.frontMatter.readingTime.time / 60000
+                </div>
+                <div className='mt-6 flex items-center'>
+                  <div className='flex w-full flex-col justify-between space-y-2 text-sm text-gray-500 sm:flex-row sm:space-x-1 sm:space-y-0'>
+                    <div className='space-x-1'>
+                      <time dateTime={article.frontMatter.date}>
+                        {prettyDate(article.frontMatter.date)}
+                      </time>
+                      {article.frontMatter.readingTime && !hideReadTime && (
+                        <>
+                          <span aria-hidden='true'>&middot;</span>
+                          <span>
+                            {Math.ceil(
+                              article.frontMatter.readingTime.time / 60000
+                            )}
+                            -minute read
+                          </span>
+                        </>
+                      )}
+                    </div>
+                    {article.frontMatter.numbers && (
+                      <div className='space-x-3'>
+                        {Object.keys(article.frontMatter.numbers).map(
+                          (name) => (
+                            <span key={name}>
+                              {article.frontMatter.numbers![name] && (
+                                <span className='inline-flex space-x-1 align-baseline'>
+                                  <span className='font-mono font-semibold text-black'>
+                                    {article.frontMatter.numbers![name]}
+                                  </span>
+                                  <span>
+                                    {article.frontMatter.numbers![name] === 1
+                                      ? name.substring(0, name.length - 1)
+                                      : name}
+                                  </span>
+                                </span>
                               )}
-                              -minute read
                             </span>
-                          </>
+                          )
                         )}
                       </div>
-                      {article.frontMatter.numbers && (
-                        <div className='space-x-3'>
-                          {Object.keys(article.frontMatter.numbers).map(
-                            (name) => (
-                              <span key={name}>
-                                {article.frontMatter.numbers![name] && (
-                                  <span className='inline-flex space-x-1 align-baseline'>
-                                    <span className='font-mono font-semibold text-black'>
-                                      {article.frontMatter.numbers![name]}
-                                    </span>
-                                    <span>
-                                      {article.frontMatter.numbers![name] === 1
-                                        ? name.substring(0, name.length - 1)
-                                        : name}
-                                    </span>
-                                  </span>
-                                )}
-                              </span>
-                            )
-                          )}
-                        </div>
-                      )}
-                    </div>
+                    )}
                   </div>
                 </div>
               </div>
-            </a>
+            </div>
           </Link>
         </li>
       ))}
