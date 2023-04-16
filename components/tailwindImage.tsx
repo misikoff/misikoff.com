9fb0899ef6717a833016522342f55fa948398c36
Thomas Misikoff <tmisikoff@gmail.com>
2023-04-16 01:53:16 -0400

    fix: tailwind unsplash imp

---
+++ components/tailwindImage.tsx
@@ -0,0 +1,43 @@
+'use client'
+
+import { useState } from 'react'
+import Image from 'next/image'
+import { clsx as classNames } from 'clsx'
+import { PhotoIcon } from '@heroicons/react/24/outline'
+
+export default function TailwindImage({
+  className = '',
+  priority = false,
+  src = '',
+  alt = '',
+  height = 100,
+  width = 100,
+  unsplash = false,
+}) {
+  const [isLoading, setLoading] = useState(true)
+  if (unsplash) {
+    src = src + `&fit=crop&w=${width}&h=${height}&q=80`
+  }
+
+  return src ? (
+    <div className='overflow-clip rounded-md'>
+      <Image
+        priority={priority}
+        src={src}
+        alt={alt}
+        height={height}
+        width={width}
+        className={classNames(
+          className,
+          'object-cover duration-700 ease-in-out group-hover:opacity-75',
+          isLoading
+            ? 'scale-110 blur-2xl grayscale'
+            : 'scale-100 blur-0 grayscale-0'
+        )}
+        onLoadingComplete={() => setLoading(false)}
+      />
+    </div>
+  ) : (
+    <PhotoIcon className={`${className} h-full w-full stroke-gray-400`} />
+  )
+}
