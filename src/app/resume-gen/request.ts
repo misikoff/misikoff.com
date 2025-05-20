bdbda7b671376e2114ca54e57f50a34336d3b31e
Thomas Misikoff <tmisikoff@gmail.com>
2025-05-20 14:20:58 -0500

    fix: improvements

---
+++ src/app/resume-gen/request.ts
@@ -0,0 +1,13 @@
+import { jobs } from '@/constants/workExperience'
+
+export function createAIRequest() {
+  console.log('Creating AI request...')
+  // create a the text of the request: it should be a string that lists all the work experiences as json objects
+  // and then asks the ai which of the experiences are relevant to the job description
+
+  // Given the following work experiences, which ones are relevant to the job description?\\n\\n
+  const requestString = `{
+  "prompt": "${JSON.stringify([...jobs], null, 2)}`
+
+  return requestString
+}
